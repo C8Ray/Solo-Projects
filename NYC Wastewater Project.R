4563ed7d2560b82f4c5906a6f73083fc2332edb3
Cait Ray <caitsray@gmail.com>
2022-03-13 14:49:26 -0400

    Got the data Listed objectives

---
+++ NYC Wastewater Project.R
@@ -0,0 +1,19 @@
+#NYC Wastewater Analysis-----
+
+#Load data and packages
+libary(tidyverse)
+readme <- read.csv("C:/Users/Caitlin Ray/Documents/Solo-Projects/COVID19_SARS-CoV-2_data_on_wastewater_samples__METADATA_V02.00.csv")
+conc <- read.csv("C:/Users/Caitlin Ray/Documents/Solo-Projects/SARS-CoV-2_concentrations_measured_in_NYC_Wastewater.csv")
+
+#1. make this a prompt for most recent download
+
+#2. remove first row/title
+
+#3. convert dates to dates
+
+#4. assign borough as new data is added
+
+#5.display data by treatment plant and by borough in a trended fashion
+
+
+
