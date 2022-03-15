241d2e7f2e54dc65ccf01b7a6cf9730b332ee8aa
Cait Ray <caitsray@gmail.com>
2022-03-15 09:20:54 -0400

    Added dialog box Made user choose file Dates are now dates, not chr Added column for Borough to update with each upload

--- NYC Wastewater Project.R
+++ NYC Wastewater Project.R
@@ -2,12 +2,21 @@
 
 #Load data and packages
 libary(tidyverse)
+install.packages("svDialogs")
+library(svDialogs)
 readme <- read.csv("C:/Users/Caitlin Ray/Documents/Solo-Projects/COVID19_SARS-CoV-2_data_on_wastewater_samples__METADATA_V02.00.csv")
 conc <- read.csv("C:/Users/Caitlin Ray/Documents/Solo-Projects/SARS-CoV-2_concentrations_measured_in_NYC_Wastewater.csv")
 
-#1. make this a prompt for most recent download
+#1. make this a prompt for most recent download AND #2. remove first row/title
+dlg_message(
+  "The next pop-up will ask that you choose a .CSV file.
+  \nPlease upload a file of the most recent download of the NYC watewater data from 
+  \nhttps://data.cityofnewyork.us/Health/SARS-CoV-2-concentrations-measured-in-NYC-Wastewat/f7dc-2q9f/data",
+  type = c("ok")
+)
 
-#2. remove first row/title
+wwdata <- read.csv(file.choose(), header=T, skip=1)
+
 
 #3. convert dates to dates
 
